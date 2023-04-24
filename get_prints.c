7b0e54bc016077fd731f81ea93233171d7ba4601
Hamza <villain.bom@gmail.com>
2023-04-24 16:37:14 -0700

    test

--- get_prints.c
+++ get_prints.c
@@ -35,8 +35,12 @@ int (*get_print(char s))(va_list, flags_t *)
 
 	register int i;
 
-for (i = 0; i < flags; i++)
-if (func_arr[i].c == s)
-	return (func_arr[i].f);
-	return (NULL);
+	for (i = 0; i < flags; i++) 
+	{
+		if (func_arr[i].c == s) 
+	{
+		return (func_arr[i].f);
+}
+}
+return (NULL);
 }
